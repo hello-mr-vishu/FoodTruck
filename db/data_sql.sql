4c95c33b70b71eb4a10de46ccd17f255b8d279c6
hello-mr-vishu <vishnurongali21@gmail.com>
2024-12-31 20:56:39 +0530

    categories Table created

--- db/data_sql.sql
+++ db/data_sql.sql
@@ -9,4 +9,16 @@
         quantity INT NOT NULL,
         delivery_address VARCHAR(255) NOT NULL,
         order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
+    );
+
+    --  Insert data into orders table
+
+    INSERT INTO orders (order_id, user_id, quantity, delivery_address) VALUES\
+    (1, 1, 2, '123 Main St'),
+(2, 2, 1, '456 Oak Rd'),
+(3, 3, 3, '789 Pine Ave');
+
+CREATE Table IF NOT EXISTS categories(
+    category_id INT PRIMARY KEY AUTOINCREMENT,
+    category_name VARCHAR(255) NOT NULL
+)
