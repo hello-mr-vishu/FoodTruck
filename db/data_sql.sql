d63fe98d635ea08df58980a6d44841ed49a80c3f
hello-mr-vishu <vishnurongali21@gmail.com>
2024-12-31 20:53:00 +0530

    orders Table created

--- db/data_sql.sql
+++ db/data_sql.sql
@@ -5,5 +5,8 @@
     CREATE TABLE IF NOT EXISTS  orders(
         order_id INT PRIMARY KEY AUTOINCREMENT,
         user_id INt ,
-        
+        item_id INT,
+        quantity INT NOT NULL,
+        delivery_address VARCHAR(255) NOT NULL,
+        order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
