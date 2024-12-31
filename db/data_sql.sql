24cb8eabbe61ed4ee9aebc41da9042f92d77e173
hello-mr-vishu <vishnurongali21@gmail.com>
2024-12-31 20:50:44 +0530

    DataBase created

---
+++ db/data_sql.sql
@@ -0,0 +1,9 @@
+    CREATE DATABASE foodtruck;
+
+    USE foodtruck;
+
+    CREATE TABLE IF NOT EXISTS  orders(
+        order_id INT PRIMARY KEY AUTOINCREMENT,
+        user_id INt ,
+        
+    )
